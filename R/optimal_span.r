c8e433cfaa45c4066c9a884a113f22663fe47255
bnasr <bijan.s.nasr@gmail.com>
2019-05-14 12:24:51 -0700

    -passing user-defined span -passing control on saving the plots -passing weights to optimal_span

--- R/optimal_span.r
+++ R/optimal_span.r
@@ -7,6 +7,7 @@
 #' @param x a vector with dates / time steps
 #' @param weights optional values to weigh the loess fit with
 #' @param step span increment size
+#' @param span customized span list or variable. if NULL, it would be set by step
 #' @param label title to be used when plotting function output
 #' @param plot plot visual output of the optimization routine
 #' @return Returns an optimal span to smooth a provided vector using 
@@ -26,16 +27,21 @@ optimal_span = function(y,
                         x = NULL,
                         weights = NULL,
                         step = 0.01,
+                        span = NULL,
                         label = NULL,
                         plot = FALSE){
-
+  
+  # parameter range
+  # manual span range
+  if(is.null(span)) span = seq(0.01, 0.5, by = step)
+  
   # custom AIC function which accepts loess regressions
   myAIC = function(x){
-
+    
     if (!(inherits(x, "loess"))){
       stop("Error: argument must be a loess object")
     }
-
+    
     # extract loess object parameters
     n = x$n
     traceL = x$trace.hat
@@ -43,39 +49,39 @@ optimal_span = function(y,
     delta1 = x$one.delta
     delta2 = x$two.delta
     enp = x$enp
-
+    
     # calculate AICc1
     # as formulated by Clifford M. Hurvich;
     # Jeffrey S. Simonoff; Chih-Ling Tsai (1998)
     AICc1 = n*log(sigma2) + n* ( (delta1/delta2)*(n+enp) /
                                    ((delta1^2/delta2)-2))
-
+    
     if(is.na(AICc1) | is.infinite(AICc1)){
       return(NA)
     }else{
       return(AICc1)
     }
   }
-
+  
   # create numerator if there is none
   if (is.null(x)){
     x = 1:length(y)
   }
-
+  
   # return AIC for a loess function with a given span
   loessAIC = function(span){
     # check if there are weights, if so use them
     if ( is.null(weights) ){
       fit = suppressWarnings(try(stats::loess(y ~ as.numeric(x),
-                                       span = span),
+                                              span = span),
                                  silent = TRUE))
     } else {
       fit = suppressWarnings(try(stats::loess(y ~ as.numeric(x),
-                                       span = span, 
-                                       weights = weights),
+                                              span = span, 
+                                              weights = weights),
                                  silent = TRUE))
     }
-
+    
     # check if the fit failed if so return NA
     if (inherits(fit, "try-error")){
       return(NA)
@@ -83,10 +89,7 @@ optimal_span = function(y,
       return(myAIC(fit))
     }
   }
-
-  # parameter range
-  span = seq(0.01, 1, by = step)
-
+  
   # temporary AIC matrix, lapply loop
   # (instead of for loop) cleaner syntax
   tmp = unlist(lapply(span, loessAIC))
@@ -110,9 +113,18 @@ optimal_span = function(y,
     col = grDevices::rainbow(length(span),alpha = 0.5)
     
     for (i in 1:length(span)){
-      fit <- try(stats::loess(y ~ as.numeric(x),
-                  span = span[i]),
-          silent = TRUE)
+      
+      # check if there are weights, if so use them
+      if(is.null(weights)){
+        fit <- try(stats::loess(y ~ as.numeric(x),
+                                span = span[i]),
+                   silent = TRUE)
+      }else{
+        fit <- try(stats::loess(y ~ as.numeric(x),
+                                weights =weights, 
+                                span = span[i]),
+                   silent = TRUE)
+      }
       
       if(!inherits(fit, "try-error")){
         graphics::lines(fit$x,
@@ -122,14 +134,21 @@ optimal_span = function(y,
       }
     }
     
-    fit = stats::loess(y ~ as.numeric(x),
-                span = opt_span)
+    # check if there are weights, if so use them
+    if(is.null(weights)){
+      fit = stats::loess(y ~ as.numeric(x),
+                         span = opt_span)
+    }else{
+      fit = stats::loess(y ~ as.numeric(x),
+                         weights =weights, 
+                         span = opt_span)
+    }
     
     graphics::lines(fit$x,
-          fit$fitted,
-          lwd = 3,
-          col = 'black',
-          lty = 1)
+                    fit$fitted,
+                    lwd = 3,
+                    col = 'black',
+                    lty = 1)
     
     plot(span,
          tmp,
@@ -141,7 +160,7 @@ optimal_span = function(y,
     graphics::abline(v = opt_span,col = 'black')
     
   }
-
+  
   # trap error and return optimal span
   if (is.na(opt_span)) {
     return(NULL)
--- R/process_phenocam.r
+++ R/process_phenocam.r
@@ -16,6 +16,8 @@
 #' @param snow_flag integrate snow flags?
 #' @param snow_weight weight given to snow flagged values, by default these are
 #' not down weighted in smoothing (default = 1)
+#' @param span customized span list or variable, default is NULL
+#' @param save_gcc90 whether to save the plot output for gcc_90 timeseries (as a test)
 #' @return Downloaded files in out_dir of requested time series products, as well
 #' as derived phenophase estimates based upon these time series.
 #' @keywords PhenoCam, Daymet, climate data, modelling, post-processing
@@ -47,7 +49,9 @@ process_phenocam <- function(
   out_dir = tempdir(),
   internal = FALSE,
   snow_flag = FALSE, 
-  snow_weight = 1
+  snow_weight = 1,
+  save_gcc90 = FALSE,
+  span = NULL
 ){
   
   # check file
@@ -75,8 +79,8 @@ process_phenocam <- function(
     
     # detect outliers
     df.tmp = try(suppressWarnings(detect_outliers(df, 
-                                              snow_flag = snow_flag)),
-             silent = TRUE)
+                                                  snow_flag = snow_flag)),
+                 silent = TRUE)
     
     # trap errors
     if(inherits(df.tmp, "try-error")){
@@ -93,8 +97,10 @@ process_phenocam <- function(
     
     # smooth time series
     df.tmp = try(suppressWarnings(smooth_ts(df, 
-                                        snow_weight = snow_weight)),
-             silent = TRUE)
+                                            span = span,
+                                            save_gcc90 = save_gcc90,
+                                            snow_weight = snow_weight)),
+                 silent = TRUE)
     
     # trap errors
     if(inherits(df.tmp,"try-error")){
@@ -113,8 +119,8 @@ process_phenocam <- function(
     # smooth time series
     phenophase_check = try(suppressWarnings(
       phenophases(data = df,
-      out_dir = out_dir,
-      internal = FALSE)),
+                  out_dir = out_dir,
+                  internal = FALSE)),
       silent = TRUE)
     
     # trap errors
@@ -131,8 +137,8 @@ process_phenocam <- function(
     
     # merge daymet data into the time series file
     df.tmp = try(merge_daymet(df,
-                          trim = trim_daymet),
-             silent = TRUE)
+                              trim = trim_daymet),
+                 silent = TRUE)
     
     # trap errors
     if(inherits(df.tmp,"try-error")){
@@ -150,7 +156,7 @@ process_phenocam <- function(
     
     # merge daymet data into the time series file
     df.tmp = try(contract_phenocam(df),
-             silent = TRUE)
+                 silent = TRUE)
     
     # trap errors
     if(inherits(df.tmp,"try-error")){
